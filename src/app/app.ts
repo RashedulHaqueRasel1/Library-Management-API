08307a659216c461776f601ebc842e54980a1551
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2025-07-18 20:00:52 +0600

    Library-Management Backend Setup with Express, TypeScript, and MongoDB (via Mongoose)

---
+++ src/app/app.ts
@@ -0,0 +1,40 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import mongoose from 'mongoose';
+// import { notesRouter } from './controllers/notes.controllers';
+// import { userRouter } from './controllers/users.controllers';
+
+
+const app: Application = express();
+
+// ✅ Must-have middleware
+app.use(express.json());
+
+
+// ✅ Register the notes router
+// app.use("/notes", notesRouter);
+// app.use("/users", userRouter);
+
+
+
+
+
+// well come route
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    res.send({ message: 'Hello, Well Come to the Library Management API System!' });
+});
+
+// 404 Not Found middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).send({
+        message: '404 Not Found',
+        error: 'The requested resource was not found'
+    });
+});
+
+// Error handling middleware
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error('❌ Error:', error.message);
+    res.status(500).send({ message: 'Internal Server Error', error: error.message });
+});
+
+export default app;
---
+++ src/app/server.ts
@@ -0,0 +1,30 @@
+import { Server } from 'http';
+import app from './app';
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+dotenv.config(); 
+
+// Load environment variables from .env file
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+ 
+let server: Server;
+const port = 3000;
+
+async function main() {
+    try {
+        await mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.gwet8mu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+        console.log('✅ Library Management Connected to MongoDB Using Mongoose');
+
+        server = app.listen(port, () => {
+            console.log(`✅ Library Management Server is running on http://localhost:${port}`);
+        });
+
+    } catch (error) {
+        console.error('❌ Error starting server:', error);
+    }
+}
+
+main();
+
